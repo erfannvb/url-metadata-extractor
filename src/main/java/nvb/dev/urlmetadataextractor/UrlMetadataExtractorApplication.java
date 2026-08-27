ed532804612324458623e9fd9a67ec844f72cdd0
erfannvb <erfannavab@gmail.com>
2026-08-27 21:35:03 +0330

    Initial commit

---
+++ src/main/java/nvb/dev/urlmetadataextractor/UrlMetadataExtractorApplication.java
@@ -0,0 +1,13 @@
+package nvb.dev.urlmetadataextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlMetadataExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlMetadataExtractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nvb/dev/urlmetadataextractor/UrlMetadataExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.dev.urlmetadataextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlMetadataExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
