28af47672058ea5b4fa8deb70180af5a92f58c47
erfannvb <erfannavab@gmail.com>
2026-08-29 19:47:59 +0330

    feat: add metadata extraction controller

---
+++ src/main/java/nvb/dev/urlmetadataextractor/controller/UrlExtractorController.java
@@ -0,0 +1,25 @@
+package nvb.dev.urlmetadataextractor.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import nvb.dev.urlmetadataextractor.dto.ExtractMetadataRequest;
+import nvb.dev.urlmetadataextractor.dto.ExtractMetadataResponse;
+import nvb.dev.urlmetadataextractor.service.UrlExtractorService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+@RequiredArgsConstructor
+public class UrlExtractorController {
+
+    private final UrlExtractorService urlExtractorService;
+
+    @PostMapping(path = "/metadata")
+    public ExtractMetadataResponse extractMetadata(@Valid @RequestBody ExtractMetadataRequest request) {
+        return null;
+    }
+
+}
